b4ebf942037b8d3ebd06f2f963b1544b2f76eaec
Sebastian <kikheylo@gmail.com>
2023-03-09 01:25:35 -0500

    feat: Creando modelos

--- src/app.js
+++ src/app.js
@@ -1,12 +1,31 @@
 const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+
 const dataBase = require("./utils/database");
+const initModel = require("./models/initModel");
 
-const PORT = 8000;
+const app = express();
 
-dataBase.authenticate()
+app.use(cors());
+app.use(morgan("dev"));
+app.use(express.json());
+
+
+
+dataBase
+  .authenticate()
   .then(() => {
     console.log("Conexión de base de datos OK");
   })
   .catch((e) => {
     console.log(e);
   });
+  
+initModel();
+
+dataBase.sync()
+.then(() => console.log("Base de datos sync"))
+.catch((error) => console.log(error));
+
+const PORT = 8000;
